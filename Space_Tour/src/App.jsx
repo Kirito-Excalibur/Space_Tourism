f834507b6e970d50a87900af2da5b3f551528a17
Nikhil <nikhilsimontoppo@gmail.com>
2022-11-10 08:40:36 +0530

    TailwindCSS set-up

--- Space_Tour/src/App.jsx
+++ Space_Tour/src/App.jsx
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from "./logo.svg";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+    <div>
+      <header>
+        <p className="text-red-900">Life is wierd</p>
       </header>
     </div>
   );
